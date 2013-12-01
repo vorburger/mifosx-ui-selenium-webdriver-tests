be3df6622ea2cfd20f4190d591a625d418624207
avikganguly01 <avikganguly010@gmail.com>
2013-12-02 01:07:35 +0530

    Login Specification

--- src/test/java/org/mifosplatform/angularui/tests/Deployments.java
+++ src/test/java/org/mifosplatform/angularui/tests/Deployments.java
@@ -1,10 +1,5 @@
 package org.mifosplatform.angularui.tests;
 
-import java.io.InputStream;
-
-import org.jboss.shrinkwrap.api.ArchivePaths;
-import org.jboss.shrinkwrap.api.Node;
-import org.jboss.shrinkwrap.api.asset.Asset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 
@@ -18,10 +13,6 @@ public class Deployments {
                     .resolve(FRONT_END_COORDINATES)
                     .withoutTransitivity()
                     .as(WebArchive.class)[0];
-        Node node = webArchive.get(ArchivePaths.create("app/scripts/modules/configurations.js"));
-        Asset asset = node.getAsset();
-        InputStream is = asset.openStream();
-        // TODO OK, so how do you know change .constant('API_URL_OVERRIDE' to true using the Arquillian API ?? 
         return webArchive;
     }
 }
